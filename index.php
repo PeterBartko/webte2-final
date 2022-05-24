b51280aa03e91aaf5b725abaffceebf455a84a54
Initial commit <xsefcikm@stuba.sk>
2022-05-24 14:27:12 +0200

    bilingualism

--- index.php
+++ index.php
@@ -21,28 +21,32 @@ $_SESSION['i'] = 0;
             integrity="sha512-NxlWEbNbTV6acWnTsWRLIiwzOw0IwHQOYUCKBiu/NqZ+5jSy7gjMbpYI+/4KvaNuZ1qolbw+Vnd76pbIUYEG8g=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/konva@8/konva.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <link rel="stylesheet" href="style.css">
     <title>Webte-final</title>
 </head>
 <body>
-<h1>Final Zadanie</h1>
+<h1 data-translate="h1"></h1>
 <div id="div-menu">
-    <button>API docs</button>
-    <button id="send">Send logs</button>
-    <label>En/SK</label>
+    <button data-translate="b1"></button>
+    <button id="send" data-translate="b2"></button>
+    <div>
+        <img src="images/flag_gb.png" onclick="change_lang_en()" height="20px" width="30px" alt="flag_gb">
+        <img src="images/flag_sk.png" onclick="change_lang_sk()" height="20px" width="30px" alt="flag_sk">
+    </div>
 </div>
 <div id="div-screen">
     <div id="input-anime">
         <input id="r-input" type="text" name="r" placeholder="r: " value="0.1">
         <div>
-            <button id="r-button" type="submit" value="Submit">Submit</button>
+            <button id="r-button" type="submit" value="Submit" data-translate="butsub"></button>
         </div>
     </div>
     <div id="div-checkbox">
         <input type="checkbox" id="check-anime" checked="checked" name="anim" value="yes">
-        <label>ANIME</label>
+        <label data-translate="lab2"></label>
         <input type="checkbox" id="check-graph" checked="checked" name="graf" value="yes">
-        <label>GRAPH</label>
+        <label data-translate="lab3"></label>
     </div>
     <div id="div-animations">
         <div id="harmon" class="HarmonicOscillator" style="visibility: visible">
@@ -57,12 +61,12 @@ $_SESSION['i'] = 0;
 
     <div id="input-calculate">
         <textarea>...</textarea>
-        <button>Calculate</button>
+        <button data-translate="but"></button>
         <textarea>...</textarea>
     </div>
 </div>
 <div>
-    <label>Names</label>
+    <label data-translate="names"></label>
 </div>
 
 <script>
@@ -84,5 +88,64 @@ $_SESSION['i'] = 0;
 </div>-->
 
      <script src="script.js"></script>
+<script>
+    var dictionary = {
+        'h1': {
+            'en': 'Final assignment',
+            'sk': 'Finálne zadanie'
+        },
+        'b1': {
+            'en': 'API docs',
+            'sk': 'API dokumentácia'
+        },
+        'b2': {
+            'en': 'Send logs',
+            'sk': 'Pošli logy'
+        },
+        'lab2': {
+            'en': 'ANIMATION',
+            'sk': 'animácia'
+        },
+        'lab3': {
+            'en': 'GRAPH',
+            'sk': 'GRAF'
+        },
+        'but': {
+            'en': 'Calculate',
+            'sk': 'Vypočítaj'
+        },
+        'butsub': {
+            'en': 'Submit',
+            'sk': 'Potvrď'
+        },
+        'names': {
+            'en': 'Names',
+            'sk': 'Mená'
+        }
+    };
+    var langs = ['en', 'sk'];
+    var current_lang_index = 0;
+    var current_lang = langs[current_lang_index];
+
+    window.change_lang_en = function() {
+        current_lang_index = 0;
+        current_lang = langs[current_lang_index];
+        translate();
+    }
+    window.change_lang_sk = function() {
+        current_lang_index = 1;
+        current_lang = langs[current_lang_index];
+        translate();
+    }
+
+    function translate() {
+        $("[data-translate]").each(function(){
+            var key = $(this).data('translate');
+            $(this).html(dictionary[key][current_lang] || "N/A");
+        });
+    }
+
+    translate();
+</script>
 </body>
 </html>
