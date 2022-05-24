4a5d3dc5f729b61186c0ee64ca50994448cad9dc
PeterBartko <peterbartko132@gmail.com>
2022-05-24 14:34:27 +0200

    textarea api

---
+++ api_ta.php
@@ -0,0 +1,33 @@
+<?php
+include 'config.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+if (isset($_GET['ta'])) {
+    $ta = $_GET['ta'];
+    $ans = [];
+    $err = 0;
+
+    exec('2>&1 octave-cli --eval "pkg load control; '. $ta .' "', $ans, $err);
+
+    function parseErr($str): string {
+        $parsed = '';
+        foreach ($str as $msg)
+            if ($msg != "")
+                $parsed .= ' ' . $msg;
+        return $parsed;
+    }
+
+    $log_date = date("d.m.Y H:i");
+    $log_value = $ta;
+    $log_isOK = $err == 0 ? 'true' : 'false';
+    $log_info = $err != 0 ? parseErr($ans) : 'success';
+    $ans = $err != 0 ? '' : $ans;
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("insert into logs (`log_date`, `input`, `log_success`, `log_info`) values (?, ? ,? ,?)");
+    $stmt->execute([$log_date, $log_value, $log_isOK, $log_info]);
+
+    echo json_encode(['answer' => $ans, 'error' => ['isOK' => $log_isOK, 'error_msg' => $log_info]]);
+}
